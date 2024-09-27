854f3c5f0f9119c4fb26eb7ad6cd3095d46e6956
Pranisha2001 <odelapranisha@gmail.com>
2024-09-27 12:48:50 +0530

    Add files via upload

---
+++ assignString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_01
+{
+    internal class assignString
+    {
+        //Assign a value "Puja" for the empName variable and print the value of empId on console
+        static void Main(string[] args)
+        {
+            // Declare a string variable named empName and initialize it with the value "Puja"
+            string empName = "Puja";
+            // Print the value stored in the empName variable
+            Console.WriteLine(empName);
+            // Wait for the user to press a key before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
